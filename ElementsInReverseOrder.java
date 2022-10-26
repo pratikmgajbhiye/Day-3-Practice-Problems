90ed6b1b70a318999b7890460e9149b08db603e9
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-26 15:32:44 +0530

    Added: Print the Elements of an Array in Reverse Order

---
+++ ElementsInReverseOrder.java
@@ -0,0 +1,15 @@
+package com.BridgeLabz.Day3PracticeProblem;
+
+public class ElementsInReverseOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = new int [] {10, 20, 30, 50, 60}; //Initialize array
+		System.out.println("Array Elements in Reverse Order: ");
+		 for (int i= arr.length-1; i>= 0; i--) {  
+	            System.out.print(arr[i] + " ");  
+	        }  
+
+	}
+
+}
